297344da25020813dfb0070eacf9aed8cb89a8cb
Pedro Cauã <pcaua6372@gmail.com>
2026-03-17 22:35:36 -0300

    Server config

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
+import ticketRoutes from "./routes/ticket.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/tickets", ticketRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
